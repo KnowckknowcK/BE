0d1811d826faec9080bddc0e5fa59d523e3f213b
kwonssshyeon <kwonsh4396@gmail.com>
2024-03-27 13:50:34 +0900

    Env : springboot 프로젝트 생성

---
+++ src/main/java/com/knu/KnowcKKnowcK/KnowcKKnowcKApplication.java
@@ -0,0 +1,13 @@
+package com.knu.KnowcKKnowcK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowcKKnowcKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowcKKnowcKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knu/KnowcKKnowcK/KnowcKKnowcKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.KnowcKKnowcK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowcKKnowcKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
