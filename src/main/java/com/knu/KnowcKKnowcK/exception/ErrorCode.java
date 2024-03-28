814b5d4663025c5c70657019d8c40e1f1cb529dc
kwonssshyeon <kwonsh4396@gmail.com>
2024-03-28 17:25:30 +0900

    Feat: custom exception handler 추가

---
+++ src/main/java/com/knu/KnowcKKnowcK/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.knu.KnowcKKnowcK.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    /**
+     * 대문자로 ErrorCode명 정의 (상태코드 / HttpStatus / 메세지)
+     */
+    USER_NOT_FOUND(404,HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
+    INVALID_PERMISSION(401,HttpStatus.UNAUTHORIZED, "권한이 없습니다");
+
+    private int status;
+    private HttpStatus error;
+    private String message;
+}
