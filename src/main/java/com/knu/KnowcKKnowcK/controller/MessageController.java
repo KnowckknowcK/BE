9a3993a20a3ef621aad84b50a484b7a449c33c70
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-03-30 15:20:41 +0900

    Comment: Swagger에 ApiResponse 예시 추가

--- src/main/java/com/knu/KnowcKKnowcK/controller/MessageController.java
+++ src/main/java/com/knu/KnowcKKnowcK/controller/MessageController.java
@@ -1,6 +1,6 @@
 package com.knu.KnowcKKnowcK.controller;
 
-import com.knu.KnowcKKnowcK.apiResponse.ApiResponse;
+import com.knu.KnowcKKnowcK.apiResponse.ApiResponseDto;
 import com.knu.KnowcKKnowcK.apiResponse.SuccessCode;
 import com.knu.KnowcKKnowcK.domain.Member;
 import com.knu.KnowcKKnowcK.dto.requestdto.PreferenceRequestDto;
@@ -11,6 +11,8 @@ import com.knu.KnowcKKnowcK.service.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -29,26 +31,40 @@ public class MessageController {
     @GetMapping("/{debateRoomId}")
     @Operation(summary = "메세지 요청 API", description = "토론방에 올라온 메세지 리스트를 받을 때 요청하는 API")
     @Parameters({@Parameter(name = "debateRoomId", description = "메세지를 받고자 하는 토론방 ID", example = "2")})
-    public ApiResponse<List<MessageResponseDto>> getMessages(@PathVariable Long debateRoomId){
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "메세지 리스트 받기 성공"),
+            @ApiResponse(responseCode = "400", description = "메세지 리스트 받기 실패")
+    })
+    public ApiResponseDto<List<MessageResponseDto>> getMessages(@PathVariable Long debateRoomId){
         member = memberRepository.findById(1L).orElse(null);
-        return ApiResponse.success(SuccessCode.OK, messageService.getMessages(member, debateRoomId));
+        return ApiResponseDto.success(SuccessCode.OK, messageService.getMessages(member, debateRoomId));
     }
 
     @GetMapping("/thread/{messageId}")
     @Operation(summary = "스레드 메세지 요청 API", description = "특정 메세지에 대한 스레드 리스트를 받을 때 요청하는 API")
     @Parameters({@Parameter(name = "messageId", description = "스레드를 받길 바라는 특정 메세지 ID", example = "3")})
-    public ApiResponse<List<MessageThreadResponseDto>> getMessageThread(@PathVariable Long messageId) {
-        return ApiResponse.success(SuccessCode.OK, messageService.getMessageThreadDtoList(messageId));
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "메세지 스레드 리스트 받기 성공"),
+            @ApiResponse(responseCode = "400", description = "메세지 스레드 리스트 받기 실패")
+    })
+    public ApiResponseDto<List<MessageThreadResponseDto>> getMessageThread(@PathVariable Long messageId) {
+        return ApiResponseDto.success(SuccessCode.OK, messageService.getMessageThreadDtoList(messageId));
     }
 
     @PutMapping("/preference/{messageId}")
-    @Operation(summary = "좋아요/싫어요 추가 API", description = "특정 메세지에 대한 좋아요/싫어요를 표시할 때 요청하는 API")
+    @Operation(summary = "동의 추가 API", description = "특정 메세지에 대한 동의를 표시할 때 요청하는 API")
     @Parameters({
-            @Parameter(name = "messageId", description = "좋아/싫어요를 추가하길 바라는 특정 메세지 ID", example = "3"),
-            @Parameter(name = "PreferenceRequestDto", description = "좋아요/싫어요 추가 요청 바디", example = "{'isLike': true}")
+            @Parameter(name = "messageId", description = "동의를 추가하길 바라는 특정 메세지 ID", example = "3"),
+            @Parameter(name = "PreferenceRequestDto", description = "좋아요 추가 요청 바디, " +
+                    "찬성에 대한 동의면 true 반대에 대한 동의면 false",
+                    example = "{'isAgree': true}")
     })
-    public ApiResponse<Double> putPreference(@PathVariable Long messageId, @RequestBody PreferenceRequestDto preferenceRequestDTO) {
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "동의 추가 성공"),
+            @ApiResponse(responseCode = "400", description = "동의 추가 실패")
+    })
+    public ApiResponseDto<Double> putPreference(@PathVariable Long messageId, @RequestBody PreferenceRequestDto preferenceRequestDTO) {
         member = memberRepository.findById(1L).orElse(null);
-        return ApiResponse.success(SuccessCode.OK, messageService.putPreference(member, messageId, preferenceRequestDTO));
+        return ApiResponseDto.success(SuccessCode.OK, messageService.putPreference(member, messageId, preferenceRequestDTO));
     }
 }
