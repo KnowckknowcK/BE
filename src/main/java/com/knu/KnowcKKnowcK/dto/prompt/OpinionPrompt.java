aa20cb1d59d04cdf59006547fad54a0dff62fd7e
kwonssshyeon <kwonsh4396@gmail.com>
2024-03-30 17:25:57 +0900

    Feat: ChatGpt 호출시 사용할 dto 와 프롬프트 정의

---
+++ src/main/java/com/knu/KnowcKKnowcK/dto/prompt/OpinionPrompt.java
@@ -0,0 +1,17 @@
+package com.knu.KnowcKKnowcK.dto.prompt;
+
+public class OpinionPrompt {
+    private String prompt;
+    private OpinionPrompt(){
+        prompt = "아래 기사 내용에 대한 내 의견을 작성한 것에 대한 평가를 해줘";
+    }
+    private static class HOLDER{
+        public static final OpinionPrompt INSTANCE = new OpinionPrompt();
+    }
+    public static OpinionPrompt getInstance(){
+        return OpinionPrompt.HOLDER.INSTANCE;
+    }
+    public String getPrompt(){
+        return prompt;
+    }
+}
