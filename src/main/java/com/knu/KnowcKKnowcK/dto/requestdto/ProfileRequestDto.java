84a39cf6220f69c6d80b69c00620ed69b0ce3db7
Bokyeong Ju <87298145+jupyter1234@users.noreply.github.com>
2024-04-03 23:14:53 +0900

    Feat: 마이페이지 수정, 요청 기능 (#31)
    * Feat: 마이페이지 컨트롤러
    
    * Feat: 견해,요약,각 피드백 테이블 생성
    Member가 견해와 요약을 참조할 수 있다. 생명주기를 Member에서 함
    
    * Feat: 프로필 GET, 프로필 수정 기능 구현
    
    * Update src/main/java/com/knu/KnowcKKnowcK/domain/OpinionFeedback.java
    
    Co-authored-by: kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
    
    * Implement: 피드백 반영
    - RequiredArgsConstructor로 생성자 삭제
    - 불필요한 import문, 어노테이션 삭제
    
    * Fix: 데이터 mapping 관계 문법 오류 수정
    
    * Refactor : Dev 수정사항 반영
    
    * Feat: 멤버 엔티티에 저장 비즈니스로직
    부모 엔티티인 멤버에 요약 저장하는 비즈니스 로직 추가
    
    * Fix : Transactional 어노테이션 추가
    member 도메인에서 saveSummary 로직 처리
    Transactional 추가하여 저장안되는 오류 해결
    
    * Update src/main/java/com/knu/KnowcKKnowcK/domain/Opinion.java
    
    Co-authored-by: kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
    
    * Implement : cascade 옵션 적용
    
    * Implement : cascade 옵션 적용
    
    * Fix : 불필요한 import문 제거
    
    ---------
    
    Co-authored-by: kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>

---
+++ src/main/java/com/knu/KnowcKKnowcK/dto/requestdto/ProfileRequestDto.java
@@ -0,0 +1,10 @@
+package com.knu.KnowcKKnowcK.dto.requestdto;
+
+import lombok.Getter;
+
+@Getter
+public class ProfileRequestDto {
+    private String name;
+    private String email;
+    private String profileImage;
+}
