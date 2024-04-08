ec830ec97af5955fbab736abd9d40ff597aa4722
adorableco <tpdusdl1001@knu.ac.kr>
2024-04-08 19:52:08 +0900

    Feat: 카테고리 별로 지문 목록 조회
    - 카테고리 별 조회 기능을 추가함
    
    related to: #4

--- src/main/java/com/knu/KnowcKKnowcK/controller/articleSummary/LoadArticlesController.java
+++ src/main/java/com/knu/KnowcKKnowcK/controller/articleSummary/LoadArticlesController.java
@@ -1,11 +1,13 @@
 package com.knu.KnowcKKnowcK.controller.articleSummary;
 
 import com.knu.KnowcKKnowcK.domain.Article;
+import com.knu.KnowcKKnowcK.enums.Category;
 import com.knu.KnowcKKnowcK.service.articleSummary.LoadArticlesService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +29,9 @@ public class LoadArticlesController {
     @Tag(name = "지문 목록 조회", description = "모든 지문을 목록 형식으로 조회할 수 있는 API")
     @ApiResponses({@ApiResponse(responseCode = "200", description = "지문 목록 조회 성공"),
                     @ApiResponse(responseCode = "400", description = "지문 목록 조회 실패")})
-    @GetMapping("/api/article/list")
-    ResponseEntity<List<Article>> loadArticles(){
-
-        return ResponseEntity.ok(loadArticlesService.loadArticles());
+    @GetMapping("/api/article/list/{category}")
+    ResponseEntity<List<Article>> loadArticles(@PathVariable @Valid Category category){
+        return ResponseEntity.ok(loadArticlesService.loadArticles(category));
     }
 
     @Operation(summary = "지문 개별 조회", description = "문해력 진단을 할 지문 1개를 id로 조회한다.")
--- src/main/java/com/knu/KnowcKKnowcK/service/articleSummary/LoadArticlesService.java
+++ src/main/java/com/knu/KnowcKKnowcK/service/articleSummary/LoadArticlesService.java
@@ -1,13 +1,14 @@
 package com.knu.KnowcKKnowcK.service.articleSummary;
 
 import com.knu.KnowcKKnowcK.domain.Article;
+import com.knu.KnowcKKnowcK.enums.Category;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface LoadArticlesService {
 
-    List<Article>  loadArticles();
+    List<Article>  loadArticles(Category category);
 
     Optional<Article> loadArticleById(Long id);
 
