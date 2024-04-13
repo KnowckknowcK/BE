382a0ce4fc9d72abcf93e88a941f6795f7483df8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-04-13 11:50:02 +0900

    Refactor: 불필요한 Builder 패턴 삭제

--- src/main/java/com/knu/KnowcKKnowcK/dto/responsedto/DebateRoomResponseDto.java
+++ src/main/java/com/knu/KnowcKKnowcK/dto/responsedto/DebateRoomResponseDto.java
@@ -1,16 +1,16 @@
 package com.knu.KnowcKKnowcK.dto.responsedto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@Builder
-@RequiredArgsConstructor
-@AllArgsConstructor
 public class DebateRoomResponseDto {
     private Double ratio;
     private Long agreeNum;
     private Long disagreeNum;
+
+    public DebateRoomResponseDto(double ratio, long agreeNum, long disagreeNum){
+        this.ratio =ratio;
+        this.agreeNum = agreeNum;
+        this.disagreeNum = disagreeNum;
+    }
 }
