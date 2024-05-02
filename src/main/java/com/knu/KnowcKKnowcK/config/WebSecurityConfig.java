d83751415aba2e18547036a7da419b0871a62988
minju26 <mjalswn26@gmail.com>
2024-05-02 19:15:53 +0900

    Feat: jwt를 위한 security 설정 추가

--- src/main/java/com/knu/KnowcKKnowcK/config/WebSecurityConfig.java
+++ src/main/java/com/knu/KnowcKKnowcK/config/WebSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,10 +22,21 @@ public class WebSecurityConfig {
         // 개발 단계에서의 임시 설정
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequest -> authorizeRequest
                         .anyRequest().permitAll()
                 )
                 .oauth2Login(Customizer.withDefaults());
+
         return httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
