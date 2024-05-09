dfe7bf452b99a46cbcfa9c7394fd529d310817c0
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-09 16:01:42 +0900

    Feat: 존재하지 않는 토론방 입장 시 생성하도록 추가

--- src/main/java/com/knu/KnowcKKnowcK/domain/DebateRoom.java
+++ src/main/java/com/knu/KnowcKKnowcK/domain/DebateRoom.java
@@ -27,6 +27,7 @@ public class DebateRoom {
 
     @Builder
     public DebateRoom(Article article){
+        this.id = article.getId();  // 애초에 기사-토론방은 1대1 관계라 id 필드가 필요 없었을 거 같아요.
         this.article = article;
         this.title = article.getTitle();
         this.agreeNum = 0L;
