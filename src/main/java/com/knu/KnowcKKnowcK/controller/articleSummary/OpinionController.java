db9c4be104edf85a60636d12758b33c7e45226de
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-13 05:25:57 +0900

    !HOTFIX: PathVariable 파라미터명 명시

--- src/main/java/com/knu/KnowcKKnowcK/controller/articleSummary/OpinionController.java
+++ src/main/java/com/knu/KnowcKKnowcK/controller/articleSummary/OpinionController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,17 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/api/opinion", produces = "application/json;charset=UTF-8")
 @Tag(name = "견해 작성 관련 API", description = "견해 관련 API")
 public class OpinionController {
 
-    @Autowired
-    private SaveOpinionService saveOpinionService;
+    private final SaveOpinionService saveOpinionService;
 
     @PostMapping("/submit")
     @Operation(summary = "요약 피드백 받기", description = "작성한 요약에 대한 AI  피드백에 제공됨")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "요약 피드백 반환 성공"),
-            @ApiResponse(responseCode = "400", description = "요약 피드백 반환 실패")})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "요약 피드백 반환 성공"),
+            @ApiResponse(responseCode = "400", description = "요약 피드백 반환 실패")
+    })
     ApiResponseDto<OpinionResponseDto> getOpinionFeedback(@RequestBody @Valid OpinionRequestDto dto){
 
         OpinionResponseDto opinionFeedback = saveOpinionService.getOpinionFeedback(dto);
