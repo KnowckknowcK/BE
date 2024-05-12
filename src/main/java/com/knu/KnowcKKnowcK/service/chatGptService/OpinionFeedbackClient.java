6d0557f545c9af73b30c64e93e069b0c828078d2
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-13 01:49:50 +0900

    Rename: 외부 API 통신 클래스 이름 변경

--- src/main/java/com/knu/KnowcKKnowcK/service/chatGptService/OpinionFeedbackClient.java
+++ src/main/java/com/knu/KnowcKKnowcK/service/chatGptService/OpinionFeedbackClient.java
@@ -1,14 +1,12 @@
 package com.knu.KnowcKKnowcK.service.chatGptService;
 
 import com.knu.KnowcKKnowcK.config.ChatGptConfig;
-import com.knu.KnowcKKnowcK.dto.prompt.SummaryPrompt;
+import com.knu.KnowcKKnowcK.dto.prompt.OpinionPrompt;
 import com.knu.KnowcKKnowcK.dto.requestdto.ChatgptRequestDto;
 import com.knu.KnowcKKnowcK.dto.responsedto.ChatgptResponseDto;
 import com.knu.KnowcKKnowcK.enums.Score;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Pair;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 
@@ -17,14 +15,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class SummaryFeedbackService implements ChatGptService{
+public class OpinionFeedbackClient implements ChatGptClient {
+
     private final ChatGptConfig chatGptConfig;
     @Override
-    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 4000), include = {ArrayIndexOutOfBoundsException.class, IllegalArgumentException.class })
-    public Pair<Score, String> callGptApi(String article, String summary) {
-
+    public Pair<Score, String> callGptApi(String article, String opinion) {
         List<ChatgptRequestDto.Message> messageList = new ArrayList<>();
-        messageList.add(new ChatgptRequestDto.Message("user", SummaryPrompt.getInstance().getPrompt() + article + summary));
+        messageList.add(new ChatgptRequestDto.Message("user", OpinionPrompt.getInstance().getPrompt() + article + opinion));
         ChatgptRequestDto requestDto = new ChatgptRequestDto(messageList);
 
         ChatgptResponseDto responseDto = chatGptConfig.gptClient()
@@ -34,20 +31,6 @@ public class SummaryFeedbackService implements ChatGptService{
                 .bodyToMono(ChatgptResponseDto.class)
                 .block();
 
-        return parsingFeedback(responseDto.getChoices().get(0).getMessage().getContent());
-
-    }
-
-
-
-    private Pair<Score, String> parsingFeedback(String content) throws ArrayIndexOutOfBoundsException {
-
-        System.out.println("content = " + content);
-        String[] split = content.split("#");
-        String score = split[0];
-        String feedback = split[1];
-
-        return Pair.of(Score.valueOf(score), feedback);
-
+        return Pair.of(Score.FAIR, responseDto.getChoices().get(0).getMessage().getContent());
     }
 }
--- src/main/java/com/knu/KnowcKKnowcK/service/chatGptService/SummaryFeedbackClient.java
+++ src/main/java/com/knu/KnowcKKnowcK/service/chatGptService/SummaryFeedbackClient.java
@@ -1,12 +1,14 @@
 package com.knu.KnowcKKnowcK.service.chatGptService;
 
 import com.knu.KnowcKKnowcK.config.ChatGptConfig;
-import com.knu.KnowcKKnowcK.dto.prompt.OpinionPrompt;
+import com.knu.KnowcKKnowcK.dto.prompt.SummaryPrompt;
 import com.knu.KnowcKKnowcK.dto.requestdto.ChatgptRequestDto;
 import com.knu.KnowcKKnowcK.dto.responsedto.ChatgptResponseDto;
 import com.knu.KnowcKKnowcK.enums.Score;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Pair;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 
@@ -15,13 +17,14 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class OpinionFeedbackService  implements ChatGptService{
-
+public class SummaryFeedbackClient implements ChatGptClient {
     private final ChatGptConfig chatGptConfig;
     @Override
-    public Pair<Score, String> callGptApi(String article, String opinion) {
+    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 4000), include = {ArrayIndexOutOfBoundsException.class, IllegalArgumentException.class })
+    public Pair<Score, String> callGptApi(String article, String summary) {
+
         List<ChatgptRequestDto.Message> messageList = new ArrayList<>();
-        messageList.add(new ChatgptRequestDto.Message("user", OpinionPrompt.getInstance().getPrompt() + article + opinion));
+        messageList.add(new ChatgptRequestDto.Message("user", SummaryPrompt.getInstance().getPrompt() + article + summary));
         ChatgptRequestDto requestDto = new ChatgptRequestDto(messageList);
 
         ChatgptResponseDto responseDto = chatGptConfig.gptClient()
@@ -31,6 +34,20 @@ public class OpinionFeedbackService  implements ChatGptService{
                 .bodyToMono(ChatgptResponseDto.class)
                 .block();
 
-        return Pair.of(Score.FAIR, responseDto.getChoices().get(0).getMessage().getContent());
+        return parsingFeedback(responseDto.getChoices().get(0).getMessage().getContent());
+
+    }
+
+
+
+    private Pair<Score, String> parsingFeedback(String content) throws ArrayIndexOutOfBoundsException {
+
+        System.out.println("content = " + content);
+        String[] split = content.split("#");
+        String score = split[0];
+        String feedback = split[1];
+
+        return Pair.of(Score.valueOf(score), feedback);
+
     }
 }
