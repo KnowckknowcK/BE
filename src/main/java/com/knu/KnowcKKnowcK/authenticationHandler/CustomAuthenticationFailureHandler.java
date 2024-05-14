9c0fde09464e30297ba715d47ed55109cb305394
minju26 <mjalswn26@gmail.com>
2024-05-15 01:34:44 +0900

    Feat: 소셜 로그인 success/failureHandler 구현

---
+++ src/main/java/com/knu/KnowcKKnowcK/authenticationHandler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,17 @@
+package com.knu.KnowcKKnowcK.authenticationHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write("구글 로그인/회원가입 실패. 관리자에게 문의하세요.");
+    }
+}
