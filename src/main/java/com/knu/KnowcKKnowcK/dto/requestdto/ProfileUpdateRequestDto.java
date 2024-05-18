e7873dd1714ed60ac4a18ac4b0210051ab9afd45
bokyeong <wntjdals0412@gmail.com>
2024-05-18 15:09:01 +0900

    Fix: 비밀번호 수정 시 암호화 나머지 필드 null 체크 로직 추가

--- src/main/java/com/knu/KnowcKKnowcK/dto/requestdto/ProfileUpdateRequestDto.java
+++ src/main/java/com/knu/KnowcKKnowcK/dto/requestdto/ProfileUpdateRequestDto.java
@@ -1,9 +1,12 @@
 package com.knu.KnowcKKnowcK.dto.requestdto;
 
+import jakarta.annotation.Nullable;
 import lombok.Getter;
 
 @Getter
 public class ProfileUpdateRequestDto {
+    @Nullable
     private String name;
+    @Nullable
     private String password;
 }
