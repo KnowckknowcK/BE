15848f1eec36e59083033cb2726838e3d63c6337
minju26 <mjalswn26@gmail.com>
2024-05-18 21:21:08 +0900

    Fix: 회원가입 시 프로필 이미지 선택으로 수정, 랜덤 디폴트 이미지 저장

--- src/main/java/com/knu/KnowcKKnowcK/service/account/AccountService.java
+++ src/main/java/com/knu/KnowcKKnowcK/service/account/AccountService.java
@@ -24,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.security.SecureRandom;
 import java.util.Optional;
+import java.util.Random;
 
 import static com.knu.KnowcKKnowcK.apiResponse.SuccessCode.CREATED_SUCCESS;
 import static com.knu.KnowcKKnowcK.exception.ErrorCode.ALREADY_REGISTERED;
@@ -64,6 +65,8 @@ public class AccountService {
 
     public HttpStatus signupWithEmail(String userInfo, MultipartFile profileImg) throws JsonProcessingException {
 
+        String profileImgUrl = "";
+
         ObjectMapper objectMapper = new ObjectMapper();
         SignupRequestDto requestDto = objectMapper.readValue(userInfo, SignupRequestDto.class);
 
@@ -72,7 +75,13 @@ public class AccountService {
         if (member.isPresent())
             return ALREADY_REGISTERED.getError();
 
-        String profileImgUrl = awsS3Util.uploadFile(profileImg);
+        if (profileImg == null || profileImg.isEmpty()) {
+            profileImgUrl = randomDefaultImg();
+        }
+        else{
+            profileImgUrl = awsS3Util.uploadFile(profileImg);
+        }
+
 
         Member newMember = Member.builder()
                 .email(requestDto.getEmail())
@@ -98,6 +107,21 @@ public class AccountService {
                     .jwt(JwtUtil.creatJWT(email, expiredAt))
                     .build();
         }
+    }
+
+    public String randomDefaultImg() {
+
+        String[] defaultImg = {
+                "https://knowckknowck-bucket.s3.ap-northeast-2.amazonaws.com/profile/profile1.png",
+                "https://knowckknowck-bucket.s3.ap-northeast-2.amazonaws.com/profile/profile2.png",
+                "https://knowckknowck-bucket.s3.ap-northeast-2.amazonaws.com/profile/profile3.png",
+                "https://knowckknowck-bucket.s3.ap-northeast-2.amazonaws.com/profile/profile4.png"
+        };
+
+        Random random = new Random();
+        int imgNumber = random.nextInt(4);
+
+        return defaultImg[imgNumber];
     }
 
     public class MemberPasswordGenerator {
