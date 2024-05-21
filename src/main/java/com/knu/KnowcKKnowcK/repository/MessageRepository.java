5015fb7241a47c3081a81edd79b6b273107f570f
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-21 22:48:23 +0900

    Feat: Message, MessageThreads, Preferences 간 JPQL 이용한 쿼리 추가

--- src/main/java/com/knu/KnowcKKnowcK/repository/MessageRepository.java
+++ src/main/java/com/knu/KnowcKKnowcK/repository/MessageRepository.java
@@ -3,11 +3,26 @@ package com.knu.KnowcKKnowcK.repository;
 
 import com.knu.KnowcKKnowcK.domain.DebateRoom;
 import com.knu.KnowcKKnowcK.domain.Message;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByDebateRoom(DebateRoom debateRoom);
+    @Query("SELECT m, COUNT(mt), COUNT(p), m.member.profileImage " +
+            "FROM Message m " +
+            "LEFT JOIN m.messageThreads mt " +
+            "LEFT JOIN m.preferences p " +
+            "WHERE m.debateRoom = :debateRoom " +
+            "GROUP BY m.id, m.member.profileImage")
+    List<Object[]> findMessagesWithCounts(@Param("debateRoom") DebateRoom debateRoom);
+
+
+    @Query("SELECT m FROM Message m LEFT JOIN FETCH m.messageThreads WHERE m.id = :id")
+    Optional<Message> findByIdWithMessageThreads(@Param("id") Long id);
 }
